78edc12311b7a690272ee56802e953f96ab7d2d6
Zeusina <kachusov_k@outlook.com>
2022-11-11 21:54:37 +0300

    write simple echo bot

--- main.py
+++ main.py
@@ -4,6 +4,21 @@ import discord
 from discord.ext import commands
 from config import token
 
+
 import youtube_dl
 import os
+intents = discord.Intents.all()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+@bot.event
+async def on_ready():
+    print('Bot is ready.')
 
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    print(f"Получено сообщение: {message.content} {message}")
+    await message.reply(f"{message.content}")
+
+bot.run(token)
