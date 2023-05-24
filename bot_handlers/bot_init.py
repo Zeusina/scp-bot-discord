92b6bac91d11d94471cce784c71f0bcc431477c1
Zeusina <kachusov_k@outlook.com>
2023-05-24 13:33:33 +0300

    Removed code from master branch

--- bot_handlers/bot_init.py
+++
@@ -1,7 +0,0 @@
-import discord
-from discord.ext import commands
-
-
-intents = discord.Intents.all()
-intents.message_content = True
-bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'), intents=intents, description="Фирменный бот для сервера DCP аномалии")
