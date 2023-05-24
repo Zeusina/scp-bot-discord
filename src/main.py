a84fe4b27d372a73b0dbfe96559abf146e71b81a
Zeusina <kachusov_k@outlook.com>
2023-05-24 14:12:38 +0300

    Moved code to /src folder

--- src/main.py
+++ src/main.py
@@ -3,17 +3,16 @@ from config import token
 import asyncio
 from bot_handlers.bot_init import bot
 from bot_handlers import music, bot_events
-import discord
-from discord.ext import commands
-
-
-import youtube_dl
-import os
+import logging
 
 
 async def main():
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger(__name__)
     async with bot:
         await bot.add_cog(music.Music(bot))
         await bot.start(token)
-
-asyncio.run(main())
+try:
+    asyncio.run(main())
+except KeyboardInterrupt:
+    print("Bot stopped by user")
