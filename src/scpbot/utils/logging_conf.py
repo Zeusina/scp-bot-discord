6893b2adb1ea2da2b6cba51a8d2b6c582f1b1c24
Zeusina <kachusov_k@outlook.com>
2023-05-25 15:17:27 +0300

    Updated logging format

--- src/scpbot/utils/logging_conf.py
+++ src/scpbot/utils/logging_conf.py
@@ -5,7 +5,5 @@ from colorama import init, Fore
 def configurate_logging():
     init()
     logging.basicConfig(level=logging.DEBUG,
-                        format=Fore.BLACK + "%(asctime)s" + Fore.CYAN + " %(levelname)s      " + Fore.RED + "%("
-                                                                                                            "funcName)s"
-                                                                                                            " %("
-                                                                                                            "message)s")
+                        format=Fore.BLACK + "%(asctime)s" + Fore.CYAN + " %(levelname)s      "
+                        + Fore.RED + "%(funcName)s" + " %(message)s")
