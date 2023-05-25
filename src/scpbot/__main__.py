6d4c7c16cb1daa589b518f193de72bb51ae45e7b
Zeusina <kachusov_k@outlook.com>
2023-05-25 08:58:01 +0300

    Written sample bot

---
+++ src/scpbot/__main__.py
@@ -0,0 +1,28 @@
+import discord
+from os import getenv
+import logging
+from scpbot.utils import logging_conf
+
+logging_conf.configurate_logging()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    logging.info(f'We have logged in as {client.user}')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+
+client.run(getenv("TOKEN"))
